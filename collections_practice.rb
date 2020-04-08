bbad37f1e1bf7000f912e1418899fd90baf0e0ea
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-08 15:19:34 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
+def sort_array_asc ary
+  ary.sort
+end
 
+def sort_array_desc ary
+  ary.sort.reverse
+end
+
+def sort_array_char_count ary
+  ary.sort { |a,b| a.length <=> b.length }
+end
+
+def swap_elements ary
+  ele_2 = ary[1]
+  ele_3 = ary[2]
+  ary[1] = ele_3
+  ary[2] = ele_2
+  ary
+end
+
+def reverse_array ary
+  ary.reverse!
+end
+
+def kesha_maker ary
+  ary.map { |x| x.to_a.map { |y| y[2] = "$" }.join() }
+end
